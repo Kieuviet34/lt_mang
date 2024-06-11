56754390ee4c1134c2774984d9557f4528eaa45c
Kieuviet34 <90097321+Kieuviet34@users.noreply.github.com>
2024-06-11 17:35:45 +0700

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    internal class Program
+    {
+        static List<Socket> client_socket = new List<Socket>();
+        static Socket server_socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        static byte[] buffer = new byte[1024];
+        static void Main(string[] args)
+        {
+            Console.Title = "Server";
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Server starting....");
+            SetUpServer();
+            Console.ReadLine();
+        }
+
+        static void SetUpServer() { 
+            server_socket.Bind(new IPEndPoint(IPAddress.Any, 5000));
+            server_socket.Listen(10);
+            server_socket.BeginAccept(new AsyncCallback(AcceptCallBack), null);
+        }
+
+        static void AcceptCallBack(IAsyncResult ar)
+        {
+            Socket socket = server_socket.EndAccept(ar);
+            client_socket.Add(socket);
+            foreach(var user in client_socket)
+            {
+                Console.WriteLine($"Client {user} connected");
+            }
+            socket.BeginReceive(buffer, 0, buffer.Length,
+                SocketFlags.None, new AsyncCallback(ReceiveCallBack), socket);
+            server_socket.BeginAccept(new AsyncCallback(AcceptCallBack), null);
+        }
+
+        static void ReceiveCallBack(IAsyncResult ar)
+        {
+            Socket socket = (Socket)ar.AsyncState;
+            int recv = socket.EndReceive(ar);
+            byte[] databuffer = new byte[recv];
+            Array.Copy(buffer, databuffer, recv);
+            string text = Encoding.UTF8.GetString(databuffer);
+            Console.WriteLine("Receive: " + text);
+
+            byte[] data= Encoding.UTF8.GetBytes(text);
+            foreach (Socket client_soc in client_socket)
+            {
+                if(client_soc != socket) client_soc.Send(data);
+            }
+            socket.BeginReceive(buffer, 0, buffer.Length,
+                SocketFlags.None, new AsyncCallback(ReceiveCallBack), socket);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    internal class Program
+    {
+        static Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        static byte[] buffer = new byte[1024];
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Title = $"{name}";
+            ConnectToServer();
+            Thread receiveThread = new Thread(ReceiveData);
+            receiveThread.Start();
+            while (true)
+            {
+                string msg = Console.ReadLine();
+                SendData($"{name}: {msg}");
+            }
+        }
+
+        static void SendData(string v)
+        {
+            byte[] buffer = Encoding.UTF8.GetBytes(v);
+            client.Send(buffer);
+        }
+
+        static void ReceiveData()
+        {
+            while (true)
+            {
+                int recv = client.Receive(buffer);
+                byte[] databuf = new byte[recv];
+                Array.Copy(buffer, databuf, recv);
+                string text = Encoding.UTF8.GetString(databuf);
+                Console.WriteLine(text);
+            }
+        }
+
+        static void ConnectToServer()
+        {
+            int attemps = 0;
+            while (!client.Connected)
+            {
+                try
+                {
+                    attemps++;
+                    client.Connect(IPAddress.Loopback, 5000);
+                }
+                catch (SocketException)
+                {
+                    Console.Clear();
+                    Console.WriteLine(attemps);
+                }
+            }
+            Console.Clear();
+            Console.WriteLine("Connected to server");
+        }
+    }
+}
